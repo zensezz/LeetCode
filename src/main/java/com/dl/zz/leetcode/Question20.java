a81ac642af9af66fd621b7cf70e4432baef6cf93
zensezz <523902878@qq.com>
2019-07-19 17:58:11 +0800

    leetcode 20 167 633

---
+++ src/main/java/com/dl/zz/leetcode/Question20.java
@@ -0,0 +1,67 @@
+package com.dl.zz.leetcode;
+
+import java.util.Stack;
+
+/**
+ *
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ *
+ * 示例 1:
+ *
+ * 输入: "()"
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: "()[]{}"
+ * 输出: true
+ * 示例 3:
+ *
+ * 输入: "(]"
+ * 输出: false
+ * 示例 4:
+ *
+ * 输入: "([)]"
+ * 输出: false
+ * 示例 5:
+ *
+ * 输入: "{[]}"
+ * 输出: true
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Author zenghzong
+ * @Since 2019/7/19
+ * @Version 1.0
+ */
+public class Question20 {
+    // 根据栈先进后出的属性，判断是否正确
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char cStack = stack.pop();
+                boolean b1 = c == ')' && cStack != '(';
+                boolean b2 = c == ']' && cStack != '[';
+                boolean b3 = c == '}' && cStack != '{';
+                if (b1 || b2 || b3) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+ 
---
+++ src/main/java/com/dl/zz/leetcode/Question633.java
@@ -0,0 +1,38 @@
+package com.dl.zz.leetcode;
+
+/**
+ * 给定一个非负整数 c ，你要判断是否存在两个整数 a 和 b，使得 a2 + b2 = c。
+ *
+ * 示例1:
+ *
+ * 输入: 5
+ * 输出: True
+ * 解释: 1 * 1 + 2 * 2 = 5
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sum-of-square-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Author zenghzong
+ * @Since 2019/7/19
+ * @Version 1.0
+ */
+public class Question633 {
+    // 先给c开方取整，减少计算范围
+    // 两头取平方，之和与c比较，大于j-1，小于i+1；直到i>j
+    public boolean judgeSquareSum(int c) {
+        int i = 0, j = (int) Math.sqrt(c);
+        while (i <= j) {
+            int powSum = i * i + j * j;
+            if (powSum == c) {
+                return true;
+            } else if (powSum > c) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+}
+ 
